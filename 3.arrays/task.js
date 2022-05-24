925aafedbedcf927fa7c1c37119325e6cb361b67
GarovAl <afreidal_staw@mail.ru>
2022-05-24 18:39:28 +0300

    modified task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,8 +1,10 @@
+'use strict';
+
 function compareArrays(arr1, arr2) {
   let result;
 
   // Ваш код
-
+  result = arr1.length === arr2.length && arr1.every((value, i) => value === arr2[i]);
   return result; // boolean
 }
 
@@ -10,6 +12,6 @@ function advancedFilter(arr) {
   let resultArr;
 
   // Ваш код
-
+  resultArr = arr.filter(item => item > 0).filter(item => item % 3 === 0).map(item => item * 10);
   return resultArr; // array
 }
